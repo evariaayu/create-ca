ccc4f3c17e86d711d80f3ef9dcfd30a745a927bc
Vancrew <im26vancrew@gmail.com>
2015-05-05 19:15:50 +0700

    update

--- crt_request.php
+++ crt_request.php
@@ -110,6 +110,46 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <![endif]-->
   </head>
   <body>
+  <nav class="navbar navbar-static-top navbar-inverse">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle Navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">OPENCA</a>
+      </div>
+
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Request CA <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+            </ul>
+          </li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Signin CA <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+            </ul>
+          </li>
+        </ul>
+
+        <ul class="nav navbar-nav navbar-right">
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> 
+            <ul class="dropdown-menu" role="menu">
+              <li class="divider"></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container-fluid">
+  </div>
 <br>
 <div class="col-md-6 col-md-offset-3">
 	<div class="panel panel-default">
